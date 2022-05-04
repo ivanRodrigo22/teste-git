97963334741fbf45acdd4155651f614191f1cf58
Ivan <ivan-ryan@hotmail.com>
2022-05-04 16:24:22 -0300

    inclusão do projeto no git

---
+++ Curso_Cum/src/test/java/CursoCum.java
@@ -0,0 +1,24 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CursoCum {
+	
+	@Given("^que eu criei o arquivo corretamente$")
+	public void que_eu_criei_o_arquivo_corretamente() throws Throwable {
+	    
+	}
+
+	@When("^executá-lo$")
+	public void executá_lo() throws Throwable {
+	   
+	}
+
+	@Then("^a especificação deve finalizar com sucesso$")
+	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
+	   
+	}
+
+
+}
